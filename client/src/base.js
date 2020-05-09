412e723ca0dccffb32e390cdca4ad14da4e7b7e6
Henry Chan <hechan@ucsd.edu>
2020-05-09 01:19:00 -0700

    Updated api key for Firebase

--- client/src/base.js
+++ client/src/base.js
@@ -1,14 +1,14 @@
 import * as firebase from "firebase";
 
 const db = firebase.initializeApp({
-    apiKey: "AIzaSyDVM7CqM0vTxdWv5ABTZflTHwoEmFWc1sk",
-    authDomain: "cse110firebase-5877e.firebaseapp.com",
-    databaseURL: "https://cse110firebase-5877e.firebaseio.com",
-    projectId: "cse110firebase-5877e",
-    storageBucket: "cse110firebase-5877e.appspot.com",
-    messagingSenderId: "1058998347390",
-    appId: "1:1058998347390:web:a033a95276f16e6fba5a2f",
-    measurementId: "G-33TB200CDN"
+    apiKey: "AIzaSyD9jl-q7A4p30mdJwfayjlHiXPakxZ4hZc",
+    authDomain: "tagit110.firebaseapp.com",
+    databaseURL: "https://tagit110.firebaseio.com",
+    projectId: "tagit110",
+    storageBucket: "tagit110.appspot.com",
+    messagingSenderId: "44092496508",
+    appId: "1:44092496508:web:6cfdf6a296511c82a707f9",
+    measurementId: "G-ZZ28HF6EW6"
 });
 
 export default db;
