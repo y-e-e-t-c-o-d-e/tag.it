c9a1809a820928e34806c200ee7f83791afb6a4f
Ronak Shah <ronakshahultimate@gmail.com>
2020-05-09 16:51:21 -0700

    Moved config to its own file

--- client/src/base.js
+++ client/src/base.js
@@ -1,15 +1,7 @@
 import * as firebase from "firebase";
+import firebaseConfig from "./firebaseConfig";
 
-const db = firebase.initializeApp({
-    apiKey: "AIzaSyD9jl-q7A4p30mdJwfayjlHiXPakxZ4hZc",
-    authDomain: "tagit110.firebaseapp.com",
-    databaseURL: "https://tagit110.firebaseio.com",
-    projectId: "tagit110",
-    storageBucket: "tagit110.appspot.com",
-    messagingSenderId: "44092496508",
-    appId: "1:44092496508:web:6cfdf6a296511c82a707f9",
-    measurementId: "G-ZZ28HF6EW6"
-});
+const db = firebase.initializeApp(firebaseConfig);
 
 export default db;
 export const provider = new firebase.auth.GoogleAuthProvider();
