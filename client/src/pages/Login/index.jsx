47880a0bddf58c8d4cbcccaec98b3858568ec968
Henry Chan <hechan@ucsd.edu>
2020-05-09 19:02:56 -0700

    Started working on login ui, added logo

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Redirect } from "react-router-dom";
 import { AuthContext } from "../../auth/Auth";
 import db, { provider } from "../../base";
+import logo from "../../assets/logo.png";
 import './style.css';
 
 const Login = ({ history }) => {
@@ -42,21 +43,29 @@ const Login = ({ history }) => {
     }
 
     return (
-        <div className="centered">
-            <h1>Log In</h1>
-            <form onSubmit={handleLogin}>
-                <label>
-                    Email
-                    <input name="email" type="email" placeholder="Email" />
-                </label>
-                <label>
-                    Password
-                    <input name="password" type="password" placeholder="Password" />
-                </label>
-                <button type="submit">Log In</button>
-                <button onClick={handleLoginWithGoogle}>Log In with Google</button>
-            </form>
-            <button onClick={redirectSignUp}>Sign Up</button>
+        <div>
+            <div className="header">
+                <img src={logo} alt="logo" height="50" />
+            </div>
+            <div className="container">
+                <h1>Log In</h1>
+                <form onSubmit={handleLogin}>
+                    <div className="inputField">
+                        <label>Email</label>
+                        <input name="email" type="email" placeholder="Email" />
+                    </div>
+
+                    <div className="inputField">
+                        <label>Password</label>
+                        <input name="password" type="password" placeholder="Password" />
+                        <a href="/" id="pwdRecover">Forgot your password?</a>
+                    </div>
+
+                    <button type="submit">Log In</button>
+                    <button onClick={handleLoginWithGoogle}>Log In with Google</button>
+                </form>
+                <button onClick={redirectSignUp}>Sign Up</button>
+            </div>
         </div>
     );
 };
