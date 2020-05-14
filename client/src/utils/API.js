a16d1b484cf4427dda8efc9d04d37c254b869a60
Daniel Truong <37765758+daniel-d-truong@users.noreply.github.com>
2020-05-14 14:09:23 -0700

    Handles creating & receiving of user object upon Signup & Login (#30)
    * Handles creating & receiving of user object upon signup/login
    
    * Restructured User Flow in React to Pass Tests

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,29 @@
 import axios from "axios";
 
 export default {
+    /** USERS */
+    getUser: function (uuid) {
+        const config = {
+            params: {
+                userUUID: uuid
+            }
+        };
+        return axios.get("/api/user", config);
+    },
+    createUser: function (name, email, uuid) {
+        const config = {
+            method: 'post', 
+            url: '/api/user',
+            data: {
+                name: name,
+                email: email,
+                uuid: uuid
+            }
+        };
+        return axios(config);
+    },
+
+    /** POSTS */
     // Returns a PROMISE Object
     getPost: function (uuid) {
         const config = {
@@ -8,6 +31,6 @@ export default {
                 postUUID: uuid
             }
         };
-        return axios.get("http://localhost:4000/api/post", config);
-    }
+        return axios.get("/api/post", config);
+    },
 }
