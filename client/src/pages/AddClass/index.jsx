1c2c06ac879c4af209552eeb1f0182630a893706
Daniel Truong <ddtruong06@gmail.com>
2020-05-16 18:07:56 -0700

    Added class stuff

---
+++ client/src/pages/AddClass/index.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useContext } from 'react';
+import './style.css';
+
+const AddClass = (props) => {
+    return (
+        <div className="addClassPage">
+            <h1>Add a Class</h1>
+        </div>
+    )
+};
+
+export default AddClass;
