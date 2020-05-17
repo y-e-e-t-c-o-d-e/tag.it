e90d6a4a23dcc6fad3810271ed97b4b4e7aa2210
Henry Chan <hechan@ucsd.edu>
2020-05-16 18:52:54 -0700

    Added button to take user to create class page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Home from './pages/Home/index';
 import Login from './pages/Login/index';
 import Signup from './pages/Signup/index';
+import ClassCreation from './pages/ClassCreation/index';
 
 // Importing Authentication
 import PrivateRoute from "./auth/PrivateRoute";
@@ -19,6 +20,7 @@ const App = () => {
           <PrivateRoute exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
+          <Route exact path="/createclass" component={ClassCreation} />
         </div>
       </Router>
     </AuthProvider>
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -3,14 +3,18 @@ import Button from '../../components/Button';
 import './style.css';
 import db from "../../base";
 
-const Home = ({currentUser, ...rest}) => {    
+const Home = ({ currentUser, history, ...rest }) => {
+    const redirectClassCreation = () => {
+        history.push("/createclass");
+    }
+
     return (
         <div className="home">
             { // TODO: Will rearrange frontend to handle the user data.
                 currentUser ? <p>{currentUser.name}</p> : <></>
             }
             <h1>Tag.it</h1>
-            <Button text="Click Me"></Button>
+            <button onClick={redirectClassCreation}>Create a Class</button>
             <button onClick={() => db.auth().signOut()}>Sign Out</button>
         </div>
     )
