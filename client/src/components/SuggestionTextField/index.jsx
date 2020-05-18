d11fc9cc85e1d198195122aead42cb9517c9dff2
Daniel Truong <37765758+daniel-d-truong@users.noreply.github.com>
2020-05-17 17:32:01 -0700

    Creates Add Classes Page with Auto Suggestion Boxes (#49)
    * Basic class stuff
    
    * Added class stuff
    
    * Created add courses page with components
    
    * More styling
    
    * Left aligned the text in suggestions
    
    * Updated class name
    
    * Set default text map

---
+++ client/src/components/SuggestionTextField/index.jsx
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+import "./style.css";
+import AutoSuggest from 'react-autosuggest';
+import ClassUtil from './ClassUtil';
+
+// Add new UTIL files here when needed
+const utilMap = {
+    "class": ClassUtil
+};
+
+const defaultTextMap = {
+    "class": "Ex. CSE110"
+};
+
+// Each prop is required to use this component
+const SuggestionTextField = ({name, options, onBlur, type}) => {
+    const [textValue, setTextValue] = useState('');
+    const [suggestions, setSuggestions] = useState([]);
+    
+    // Changes state value when text field value updates
+    const onChange = (e, {newValue}) => {
+        setTextValue(newValue);
+    };
+
+    // Input for suggestion text boxes
+    const inputProps = {
+        value: textValue,
+        placeholder: defaultTextMap[type],
+        onChange: onChange,
+        onBlur: onBlur
+    };
+
+    // Specifies what util methods to use 
+    let util = utilMap[type];
+
+    return (
+        <div className="suggestionTextField">
+            <p>{name}</p>
+            <AutoSuggest
+                suggestions={suggestions}
+                inputProps={inputProps}
+                onSuggestionsFetchRequested={(value) => { 
+                    setSuggestions(util.getSuggestions(value, options)); 
+                }}
+                onSuggestionsClearRequested={() => { setSuggestions([]); }}
+                getSuggestionValue={util.getSuggestionValue}
+                renderSuggestion={util.renderSuggestion}
+            />
+        </div>
+    );
+};
+
+export default SuggestionTextField;
