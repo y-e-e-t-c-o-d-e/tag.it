7a5f6481ca22a318e2abbd9a47fd33b896e3324c
Daniel Truong <37765758+daniel-d-truong@users.noreply.github.com>
2020-05-17 19:28:09 -0700

    Fixing Netlify Issue (#51)
    * Removed package-lock files
    
    * Added redirects file
    
    * Redirects invalid URLs to home
    
    * Add switch so only one route is chosen
    
    * Removes return null

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 
 // Importing Pages
@@ -17,10 +17,15 @@ const App = () => {
     <AuthProvider>
       <Router>
         <div>
-          <PrivateRoute exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/add" component={AddClass} />
+          <Switch>
+            <PrivateRoute exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/add" component={AddClass} />
+            <Route exact path="/*" component={() => {
+              window.location.href = "/";
+            }} />
+          </Switch>
         </div>
       </Router>
     </AuthProvider>
