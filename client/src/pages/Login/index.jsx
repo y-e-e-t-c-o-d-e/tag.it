05fb3f7978137f68a216810a409975e48853d2b8
Henry Chan <hechan@ucsd.edu>
2020-05-19 10:25:06 -0700

    Added code for passwordless auth, still wip

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -11,6 +11,25 @@ var bgColors = {
     "error": "#ffcccc",
 };
 
+
+var actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be whitelisted in the Firebase Console.
+    url: 'https://tagdotit.netlify.app',
+    // This must be true.
+    handleCodeInApp: true,
+    iOS: {
+        bundleId: 'com.example.ios'
+    },
+    android: {
+        packageName: 'com.example.android',
+        installApp: true,
+        minimumVersion: '12'
+    },
+    dynamicLinkDomain: 'example.page.link'
+};
+
+
 const Login = ({ history }) => {
     const redirectSignUp = () => {
         history.push("/signup")
@@ -19,22 +38,59 @@ const Login = ({ history }) => {
     /* Checks whether the input form is valid */
     const [emailValid, setEmailValid] = useState(false);
 
-    const handleLogin = async (event) => {
+    const sendLink = async (event) => {
         if (emailValid) {
             event.preventDefault();
-            const { email, password } = event.target.elements;
-
-            try {
-                await db
-                    .auth()
-                    .signInWithEmailAndPassword(email.value,
-                        password.value);
-                history.push("/");
-            } catch (error) {
-                alert(error);
-            }
+            console.log('hi')
+            const { email } = event.target.elements;
+            console.log(email.value)
+            db.auth().sendSignInLinkToEmail(email.value, actionCodeSettings)
+                .then(function () {
+                    // The link was successfully sent. Inform the user.
+                    // Save the email locally so you don't need to ask the user for it again
+                    // if they open the link on the same device.
+                    window.alert('A login email was sent!');
+                    window.localStorage.setItem('emailForSignIn', email.value);
+                })
+                .catch(function (error) {
+                    // Some error occurred, you can inspect the code: error.code
+                    window.alert(error.message)
+                });
         }
         else { alert("Invalid email address"); }
+    }
+
+    const confirmSignIn = () => {
+        // Confirm the link is a sign-in with email link.
+        if (db.auth().isSignInWithEmailLink(window.location.href)) {
+            // Additional state parameters can also be passed via URL.
+            // This can be used to continue the user's intended action before triggering
+            // the sign-in operation.
+            // Get the email if available. This should be available if the user completes
+            // the flow on the same device where they started it.
+            let email = window.localStorage.getItem('emailForSignIn');
+            if (!email) {
+                // User opened the link on a different device. To prevent session fixation
+                // attacks, ask the user to provide the associated email again. For example:
+                email = window.prompt('Please provide your email for confirmation');
+            }
+            // The client SDK will parse the code from the link for you.
+            db.auth().signInWithEmailLink(email, window.location.href)
+                .then(function (result) {
+                    // Clear email from storage.
+                    window.localStorage.removeItem('emailForSignIn');
+                    // You can access the new user via result.user
+                    // Additional user info profile not available via:
+                    // result.additionalUserInfo.profile == null
+                    // You can check if the user is new or existing:
+                    // result.additionalUserInfo.isNewUser
+                })
+                .catch(function (error) {
+                    // Some error occurred, you can inspect the code: error.code
+                    // Common errors could be invalid email and invalid or expired OTPs.
+                    window.alert(error.message)
+                });
+        }
     }
 
     /* Makes possible email input background color change  */
@@ -73,7 +129,7 @@ const Login = ({ history }) => {
 
     return (
         <div>
-            <LoginNav/>
+            <LoginNav />
             <div className="container">
                 <div id="center-logo">
                     <img src={logo} alt="Tag.it" height="125" />
@@ -82,7 +138,7 @@ const Login = ({ history }) => {
                 <div className="input-row">
                     <div className="input">
                         <h2>Log In</h2>
-                        <form onSubmit={handleLogin}>
+                        <form onSubmit={sendLink}>
                             <div className="inputField">
                                 <label>Email</label>
                                 <input name="email" type="email" placeholder="Email"
