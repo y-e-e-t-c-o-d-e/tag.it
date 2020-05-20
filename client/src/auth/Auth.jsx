91cbae0a78ca0f139dec86ce57d408da9f1fa712
Daniel Truong <ddtruong06@gmail.com>
2020-05-19 20:25:23 -0700

    Finished email auth

--- client/src/auth/Auth.jsx
+++ client/src/auth/Auth.jsx
@@ -7,7 +7,22 @@ export const AuthProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
 
     useEffect(() => {
-        db.auth().onAuthStateChanged(setCurrentUser);
+        db.auth().onAuthStateChanged((user) => {
+            // User is signed in 
+            if (user) {
+                // Sets current user only if user is signed in
+                if (user.emailVerified) {
+                    setCurrentUser(user);
+                } else {
+                    // Handles when user is signed in BUT email is not verified
+                    window.alert(`Email ${user.email} needs to be verified.`);
+                    db.auth().signOut();
+                }
+            } else {
+                // User is signed out
+                setCurrentUser(user);
+            }
+        });
     }, []);
 
     return (
--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -6,29 +6,10 @@ import LoginNav from "../../components/LoginNav";
 import logo from "../../assets/logo.png";
 import './style.css';
 
-var bgColors = {
+const bgColors = {
     "default": "white",
     "error": "#ffcccc",
 };
-
-
-var actionCodeSettings = {
-    // URL you want to redirect back to. The domain (www.example.com) for this
-    // URL must be whitelisted in the Firebase Console.
-    url: 'https://tagdotit.netlify.app',
-    // This must be true.
-    handleCodeInApp: true,
-    iOS: {
-        bundleId: 'com.example.ios'
-    },
-    android: {
-        packageName: 'com.example.android',
-        installApp: true,
-        minimumVersion: '12'
-    },
-    dynamicLinkDomain: 'example.page.link'
-};
-
 
 const Login = ({ history }) => {
     const redirectSignUp = () => {
@@ -38,59 +19,27 @@ const Login = ({ history }) => {
     /* Checks whether the input form is valid */
     const [emailValid, setEmailValid] = useState(false);
 
-    const sendLink = async (event) => {
+    const handleLogin = async (event) => {
         if (emailValid) {
             event.preventDefault();
-            console.log('hi')
-            const { email } = event.target.elements;
-            console.log(email.value)
-            db.auth().sendSignInLinkToEmail(email.value, actionCodeSettings)
-                .then(function () {
-                    // The link was successfully sent. Inform the user.
-                    // Save the email locally so you don't need to ask the user for it again
-                    // if they open the link on the same device.
-                    window.alert('A login email was sent!');
-                    window.localStorage.setItem('emailForSignIn', email.value);
-                })
-                .catch(function (error) {
-                    // Some error occurred, you can inspect the code: error.code
-                    window.alert(error.message)
-                });
+            const { email, password } = event.target.elements;
+
+            try {
+                // This action will cause the onChangeEventListener to occur (in Auth.jsx)
+                const {user} = await db
+                    .auth()
+                    .signInWithEmailAndPassword(email.value,
+                        password.value);
+
+                // Only reroutes to new page if user's email is verified
+                if (user.emailVerified) {
+                    history.push("/");
+                }
+            } catch (error) {
+                alert(error);
+            }
         }
         else { alert("Invalid email address"); }
-    }
-
-    const confirmSignIn = () => {
-        // Confirm the link is a sign-in with email link.
-        if (db.auth().isSignInWithEmailLink(window.location.href)) {
-            // Additional state parameters can also be passed via URL.
-            // This can be used to continue the user's intended action before triggering
-            // the sign-in operation.
-            // Get the email if available. This should be available if the user completes
-            // the flow on the same device where they started it.
-            let email = window.localStorage.getItem('emailForSignIn');
-            if (!email) {
-                // User opened the link on a different device. To prevent session fixation
-                // attacks, ask the user to provide the associated email again. For example:
-                email = window.prompt('Please provide your email for confirmation');
-            }
-            // The client SDK will parse the code from the link for you.
-            db.auth().signInWithEmailLink(email, window.location.href)
-                .then(function (result) {
-                    // Clear email from storage.
-                    window.localStorage.removeItem('emailForSignIn');
-                    // You can access the new user via result.user
-                    // Additional user info profile not available via:
-                    // result.additionalUserInfo.profile == null
-                    // You can check if the user is new or existing:
-                    // result.additionalUserInfo.isNewUser
-                })
-                .catch(function (error) {
-                    // Some error occurred, you can inspect the code: error.code
-                    // Common errors could be invalid email and invalid or expired OTPs.
-                    window.alert(error.message)
-                });
-        }
     }
 
     /* Makes possible email input background color change  */
@@ -138,7 +87,7 @@ const Login = ({ history }) => {
                 <div className="input-row">
                     <div className="input">
                         <h2>Log In</h2>
-                        <form onSubmit={sendLink}>
+                        <form onSubmit={handleLogin}>
                             <div className="inputField">
                                 <label>Email</label>
                                 <input name="email" type="email" placeholder="Email"
