ef1316f02861bcd480caefe7993ab4f8dd99898d
Daniel Truong <ddtruong06@gmail.com>
2020-05-23 20:04:35 -0700

    Set up files

---
+++ client/src/components/CourseRoute/index.jsx
@@ -0,0 +1,10 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const CourseRoute = ({ component: RouteComponent, ...rest }) => {
+    return <Route>
+        
+    </Route>
+};
+
+export default CourseRoute;
