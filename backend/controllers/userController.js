5e7cd8e9978b0aeeb5d3207e4ac2c62310ea517a
Daniel Truong <ddtruong06@gmail.com>
2020-05-29 15:45:52 -0700

    Deletes comments

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,25 +24,6 @@ exports.addUser = async (req, res) => {
 };
 
 exports.getUser = async (req, res) => {
-    // const userUUID = req.query.userUUID;
-    // if (!userUUID) {
-    //     res.status(422).json({
-    //         status: 422,
-    //         error: "Missing paramater: userUUID"
-    //     });
-    //     return;
-    // };
-
-    // // Grabs the user based on the userUUID. If fails, responds with an error.
-    // try {
-    //     const userObj = await user.getUserById(userUUID);
-    //     res.status(200).json(userObj.props);
-    // } catch (e) {
-    //     res.status(410).json({
-    //         status: 410,
-    //         error: e
-    //     });
-    // };
     res.status(200).json(req.user.props);
 };
 
