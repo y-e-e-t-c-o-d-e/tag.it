09ca292b9c31e56e1f967da2b1829d0b82e4ae7d
Ronak Shah <ronakshahultimate@gmail.com>
2020-05-30 02:08:19 -0700

    ready for backend

--- client/src/components/PostCreator/index.jsx
+++ client/src/components/PostCreator/index.jsx
@@ -1,31 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css';
-import { Dropdown, DropdownButton, Button } from 'react-bootstrap';
+import { Dropdown, DropdownButton, Button, Form, Row, Col } from 'react-bootstrap';
 import PostEditor from "../PostEditor";
 
 const PostCreator = ({}) => {
+    const [tags, setTags] = useState([{name: "sample tag", uuid: "jlkd8f2348"}]); // all possible tags
+    const [addedTags, setAddedTags] = useState(new Set()); // tags that have been added to this post
+    const [visibility, setVisiblity] = useState("public, visible");
 
     return (
         <div className="post-creator">
-            <h3>What is your question?</h3>
-            <div className="question">
-                <PostEditor />
-            </div>
-            <h3>Please enter a description:</h3>
-            <div className="description">
-                <PostEditor />
-            </div>
-            <div className="post-buttons">
-                <h3>Post as:</h3>
-                <DropdownButton id="dropdown-button-privacy" title="public, visible" drop="up">
-                    <Dropdown.Item as="button">public, visible</Dropdown.Item>
-                    <Dropdown.Item as="button">public, anonymous</Dropdown.Item>
-                    <Dropdown.Item as="button">private</Dropdown.Item>
-                </DropdownButton>
-                <DropdownButton id="dropdown-button-tags" title="tag.it" drop="up">
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Question Summary</Form.Label>
+                    <Form.Control type="value" placeholder="How do I build a rock from sand?" />
+                </Form.Group>
 
-                </DropdownButton>
-                <Button id="create-button">create.it</Button>
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control as="textarea" rows="5" />
+                </Form.Group> 
+            </Form>
+            
+            <div className="post-buttons">
+                <Row>
+                    <Col md={3}>
+                        <h3>Post as:</h3>
+                    </Col>
+                    <Col md={3}>
+                        <DropdownButton id="dropdown-button-form" title={visibility} drop="up">
+                            <Dropdown.Item as="button" onClick={() => { setVisiblity("public, visible")}} >public, visible</Dropdown.Item>
+                            <Dropdown.Item as="button" onClick={() => { setVisiblity("public, anonymous")}} >public, anonymous</Dropdown.Item>
+                            <Dropdown.Item as="button" onClick={() => { setVisiblity("private")}} >private</Dropdown.Item>
+                        </DropdownButton>
+                    </Col>
+                    <Col md={3}>
+                        <DropdownButton id="dropdown-button-tags" title="tag.it" drop="up">
+                            { 
+                                tags.map((tag, key) => {
+                                    return <Dropdown.Item key={key} as="button" onClick={() => {
+                                        const newAddedTags = new Set(addedTags);
+                                        if (addedTags.has(tag)) {
+                                            newAddedTags.delete(tag);
+                                        } else {
+                                            newAddedTags.add(tag);
+                                        }
+                                        setAddedTags(newAddedTags);
+                                    }}>{tag.name}</Dropdown.Item>
+                                })
+                            }
+                        </DropdownButton>
+                    </Col>
+                    <Col md={3}>
+                        <Button id="create-button">create.it</Button>
+                    </Col>
+                </Row>
             </div>
         </div>
     );
