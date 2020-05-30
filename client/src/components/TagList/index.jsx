34aeab5149df44221ead273dddaeb4980b404d38
Ronak Shah <ronakshahultimate@gmail.com>
2020-05-30 03:20:18 -0700

    removed comments

--- client/src/components/TagList/index.jsx
+++ client/src/components/TagList/index.jsx
@@ -46,38 +46,4 @@ const TagList = ({}) => {
     );
 }
 
-// export class TagList extends React.Component {
-
-//     // Tags
-//     state = {
-//         tags: hctags
-//     };
-
-//     // Display all tags
-//     showTags = tags => {
-//         return tags.map(tag => {
-//             return <EachTag tag={tag} key={tag.id} />;
-//         });
-//     }
-
-//     render() {
-//         return (
-//             <div className="tag-section">
-//                 <input className="search" type="text" placeholder="Search..." />
-//                 <button className="btn-style search-btn"><b>search.it</b></button>
-//                 <div className="filter-dropdown">
-//                     <button className="btn-style filter-btn"><b>filter.it</b></button>
-//                     <div className="filter-dropdown-content"> 
-//                         <button ><b>Pinned</b></button>
-//                         <button ><b>Most Liked</b></button>
-//                     </div>
-//                 </div>
-//                 <div className="tagList">
-//                     {this.showTags(this.state.tags)}
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
-
 export default TagList
