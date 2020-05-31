3c4d989956e5f3d70b72ae4fffc385fff6e7f69c
YiyueMaggieMao <myykitty711@gmail.com>
2020-05-31 05:41:09 -0700

    Post display done

--- client/src/pages/PostView/index.jsx
+++ client/src/pages/PostView/index.jsx
@@ -4,7 +4,7 @@ import './style.css';
 import db from "../../base";
 import Navigation from "../../components/Navbar";
 import TagList from "../../components/TagList/index.jsx";
-import { Row, Col, Nav, Button } from 'react-bootstrap';
+import { Container, Row, Col, Nav, Button, Dropdown, DropdownButton } from 'react-bootstrap';
 import CoursesView from '../../components/CoursesView/index.jsx';
 import QuestionList from "../../components/QuestionList"
 import PostCreator from "../../components/PostCreator"
@@ -12,7 +12,7 @@ import { API, createToast } from '../../utils';
 import PostEditor from '../../components/PostEditor';
 import ReactMarkdown from 'react-markdown';
 
-const PostView = ({currentUser, match}) => { 
+const PostView = ({ currentUser, match }) => {
     const { postId, courseId } = useParams();
 
     const [post, setPost] = useState({
@@ -53,18 +53,56 @@ const PostView = ({currentUser, match}) => {
             <Navigation currentUser={currentUser} />
             <div className="cont">
                 <Button href={`/courses/${courseId}`}>Course Home</Button>
-            <Row>
-                <Col xs={4}>
-                    <TagList tags={tags} />
-                </Col>
-                <Col>
-                    <Row>
-                        <Col xs={8}><h1>{post.title}</h1></Col>
-                    </Row>
+                <Row>
+                    <Col xs={4}>
+                        <TagList tags={tags} />
+                    </Col>
+                    <Col>
 
-                    <ReactMarkdown className="liveView" source={post.content} />
-                </Col>
-            </Row>
+                        <div className="post-viewer">
+                            <div className="post-title-section">
+                                <Row>
+                                    <Col xs={8}>
+                                        <div className="post-title-view">
+                                            {post.title}
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <div className="title-button-section">
+                                    <Button className="yellow-button">change.it</Button>
+                                    <DropdownButton className="yellow-button" title="actions">
+                                        <Dropdown.Item key="follow" as="button">Follow Post</Dropdown.Item>
+                                        <Dropdown.Item key="copy-link" as="button">Copy Link</Dropdown.Item>
+                                        <Dropdown.Item key="resolve" as="button">Resolve</Dropdown.Item>
+                                    </DropdownButton>
+                                </div>
+                            </div>
+                            <div className="post-content-section">
+                                <ReactMarkdown className="post-content-view" source={post.content} />
+                                <div className="posted-by">Posted by: {post.author}</div>
+                            </div>
+                            <Container className="post-view-buttons">
+                                <Row>
+                                    <Col xs={6} md={4}>
+                                        <Row>
+                                            <div className="likes">18</div>
+                                            <Button className="yellow-button">like.it</Button>
+                                        </Row>
+
+                                        <Row>
+                                            <Button className="yellow-button">discuss.it</Button>
+                                        </Row>
+
+                                    </Col>
+                                    <Col>
+                                        Hi...?
+                            </Col>
+                                </Row>
+                            </Container>
+
+                        </div>
+                    </Col>
+                </Row>
             </div>
             <h1></h1>
         </div>
