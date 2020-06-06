2a64022095f2ba26f67b9013bd8cd837422dfa01
Daniel Truong <ddtruong06@gmail.com>
2020-06-06 15:23:57 -0700

    Fixed small things

--- client/src/components/PostGrid/index.jsx
+++ client/src/components/PostGrid/index.jsx
@@ -5,7 +5,7 @@ const PostGrid = (props) =>{
     return(
         <div className="post-grid">
             <div className="postview-center">
-                <a href="#" onClick={() => {props.history.push(props.link)}}> {props.title} </a>
+                <a onClick={() => {props.history.push(props.link)}} href="javascript:;"> {props.title} </a>
             </div>
         </div>
     );
